b0fdb164767f45c77b75ce188cd0e22b55dcfd60
BrianS <brianseko@gmail.com>
2025-01-03 15:54:26 -0500

    jan 2025 update

--- lib/load_creds.py
+++ lib/load_creds.py
@@ -5,7 +5,7 @@ def load_env_credentials():
     Sets variables in .env file for current session.
     :return: None
     """
-    with open("C:/Users/brian/Desktop/git repos/gas_forecasting/.env", "r") as f:
+    with open(r"C:\Users\brian\OneDrive\Desktop\git repos\gas_forecasting\.env", "r")as f:
         for line in f.readlines():
             try:
                 key, value = line.split('=')
